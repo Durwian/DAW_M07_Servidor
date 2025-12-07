6504d0e8622314e9ae8a7bf55f02ea7a25233eca
Durwian <vicforner@gmail.com>
2025-12-08 00:31:48 +0100

    started

--- dawm07eac6rendacatalunyaEnunciat/src/main/java/cat/xtec/ioc/domain/Renda.java
+++ dawm07eac6rendacatalunyaEnunciat/src/main/java/cat/xtec/ioc/domain/Renda.java
@@ -5,11 +5,52 @@ import java.util.Objects;
 public class Renda {
 
     private int any;
-    
-    public Renda(int any) { //afegir les altres propietats: homes, dones, total, origen
-        this.any = any;
+    private int homes;
+    private int dones;
+    private int total;
+    private String origen;
 
+    public Renda(int any, int homes, int dones, int total, String origen) { //afegir les altres propietats: homes, dones, total, origen
+        this.any = any;
+        this.homes = homes;
+        this.dones = dones;
+        this.total = total;
+        this.origen = origen;
     }
+    
+    public int getHomes() {
+        return homes;
+    }
+
+    public void setHomes(int homes) {
+        this.homes = homes;
+    }
+
+    public int getDones() {
+        return dones;
+    }
+
+    public void setDones(int dones) {
+        this.dones = dones;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+    
+    
 
     public int getAny() {
         return any;
--- dawm07eac6rendacatalunyaEnunciat/src/main/java/cat/xtec/ioc/domain/repository/impl/InMemoryRendaRepository.java
+++ dawm07eac6rendacatalunyaEnunciat/src/main/java/cat/xtec/ioc/domain/repository/impl/InMemoryRendaRepository.java
@@ -14,11 +14,24 @@ public class InMemoryRendaRepository implements RendaRepository {
 
     public InMemoryRendaRepository() {
         //TODO
+        Renda first = new Renda(2023, 14000, 15000, 14500, "Catalunya");
+        Renda second = new Renda(2023, 15000, 16000, 15500, "Espanya");
+        rendes.add(first);
+        rendes.add(second);
     }
     
     @Override
     public List<Renda> getAll() {
         return rendes;
+    }
+    
+    public Renda getRendaByAny(int any){
+        for(Renda renda : rendes){
+            if(renda.getAny() == any){
+                return renda;
+            }
+        }
+        return null;
     }
     
 }
